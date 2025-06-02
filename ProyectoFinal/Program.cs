326bccde244113eadd0f29b7166e9e224dce4940
Benjamin <05737875+israelAja@users.noreply.github.com>
2025-06-02 12:19:11 -0500

    equidee

--- ProyectoFinal/Program.cs
+++ ProyectoFinal/Program.cs
@@ -11,15 +11,13 @@ namespace ProyectoFinal
         static void Main(string[] args)
         {
             //comentario
-<<<<<<< HEAD
+
             //cambio2
-=======
+
             Console.WriteLine("benja");
-<<<<<<< HEAD
+
             Console.WriteLine("Jhan");
-=======
->>>>>>> 3a0929554d001709927935d4ea30dac58d2af969
->>>>>>> ee2c24ea208579db9c65d79d0def4075023b5974
+            // comentario 3
         }
     }
 }
